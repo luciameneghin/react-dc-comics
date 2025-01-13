7b35ac309d0da46e85a5a0a83fc2f58606d44594
luciameneghin <lucia.meneghin00@gmail.com>
2025-01-13 15:01:46 +0100

    feat: header with css + add: bootstrap cdn and font from google fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 
 const Header = () => {
   return (
-    <div>
-      H
-    </div>
+    <header>
+      <div className="logo-header">
+        <img src="src/img/dc-logo.png" alt="logo DC" />
+      </div>
+      <div className="list-header">
+        <lu className="">
+          <li><a href="#">characters</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </lu>
+      </div>
+    </header>
   )
 }
 

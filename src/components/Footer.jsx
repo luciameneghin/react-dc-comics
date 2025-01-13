d09f73e1ce06d360111eb6d21f20527981a83722
luciameneghin <lucia.meneghin00@gmail.com>
2025-01-13 14:23:52 +0100

    build: add base project files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div>
+      F
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      H
+    </div>
+  )
+}
+
+export default Header

2d6aa157356de0d8c03855e49c7efcfd5ac7f8c3
luciameneghin <lucia.meneghin00@gmail.com>
2025-01-15 15:47:11 +0100

    feat: add header and footer menu objects in .js files and import in App.js fix: jumbotron img position (css)

---
+++ src/data/menusFooter.js
@@ -0,0 +1,99 @@
+const footerInfo = [
+  {
+    id: 1,
+    imgSrc: "src/img/buy-comics-digital-comics.png",
+    alt: "logo digital comics",
+    text: "digital comics"
+  },
+  {
+    id: 2,
+    imgSrc: "src/img/buy-comics-merchandise.png",
+    alt: "logo digital comics",
+    text: "dc merchandise"
+  },
+  {
+    id: 3,
+    imgSrc: "src/img/buy-comics-subscriptions.png",
+    alt: "logo digital comics",
+    text: "subscription"
+  },
+  {
+    id: 4,
+    imgSrc: "src/img/buy-comics-shop-locator.png",
+    alt: "logo digital comics",
+    text: "comic shop locator"
+  },
+  {
+    id: 5,
+    imgSrc: "src/img/buy-dc-power-visa.svg",
+    alt: "logo digital comics",
+    text: "dc power visa"
+  }
+];
+
+
+
+
+const FooterMenuDataOne = [
+  {
+    id: 1,
+    title: "DC comics",
+    items: [
+      "characters",
+      "comics",
+      "movies",
+      "TV",
+      "Games",
+      "Videos",
+      "News"
+    ]
+  },
+  {
+    id: 2,
+    title: "shop",
+    items: [
+      "Shop DC",
+      "Shop DC Collectibles"
+    ]
+  }
+];
+
+const FooterMenuDataTwo = [
+  {
+    id: 3,
+    title: "dc",
+    items: [
+      "term of use",
+      "privacy policy (new)",
+      "ad choices",
+      "advertising",
+      "jobs",
+      "subscription",
+      "talent workshops",
+      "CPSC certificates",
+      "ratings",
+      "shop help",
+      "contact us"
+    ]
+  }
+]
+const FooterMenuDataThree = [
+  {
+    id: 4,
+    title: "sites",
+    items: [
+      "DC",
+      "MAD magazines",
+      "DC Kids",
+      "DC Universe",
+      "DC Power Visa"
+    ]
+  }
+]
+
+module.exports = {
+  footerInfo,
+  FooterMenuDataOne,
+  FooterMenuDataTwo,
+  FooterMenuDataThree
+}
